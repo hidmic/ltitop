ec487c31802f273d571c68d4dd35d508a547cc0c
Michel Hidalgo <hid.michel@gmail.com>
2021-06-06 17:38:45 -0300

    Update lowpass SISO filter optimization example
    - Use PSDs for frequency response assessment
    - Add more and better visualizations
    
    Signed-off-by: Michel Hidalgo <hid.michel@gmail.com>

--- examples/optimize_lowpass_siso_filter.py
+++ examples/optimize_lowpass_siso_filter.py
@@ -31,6 +31,9 @@ import deap.algorithms
 import deap.gp
 import deap.tools
 
+import matplotlib
+matplotlib.rcParams['figure.figsize'] = (10.24, 7.68)
+matplotlib.rcParams['font.size'] = 12
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
 import mpl_toolkits.mplot3d
@@ -81,8 +84,8 @@ class Criteria:
 
 
 def main():
-    random.seed(11)
-    np.random.seed(11)
+    random.seed(7)
+    np.random.seed(7)
 
     fs = 200  # Hz
     fo = 40  # Hz
@@ -90,16 +93,18 @@ def main():
     rs = 80  # dB
     fpass = fo - 5  # Hz
     fstop = fo + 5  # Hz
-    N, fo = signal.cheb1ord(fpass, fstop, rp, rs, fs=fs)
+    # N, fo = signal.cheb1ord(fpass, fstop, rp, rs, fs=fs)
     # N, fo = signal.cheb2ord(fpass, fstop, rp, rs, fs=fs)
     # N, fo = signal.buttord(fpass, fstop, rp, rs, fs=fs)
+    N, fo = signal.ellipord(fpass, fstop, rp, rs, fs=fs)
     wp = 2 * fs * np.tan(np.pi * fo / fs)
-    prototype = signal.lti(*signal.cheb1ap(N, rp))
+    # prototype = signal.lti(*signal.cheb1ap(N, rp))
     # prototype = signal.lti(*signal.cheb2ap(N, rs))
     # prototype = signal.lti(*signal.buttap(N))
-    # prototype = signal.lti(*signal.ellipap(N=N, rp=rp, rs=rs))
+    prototype = signal.lti(*signal.ellipap(N=N, rp=rp, rs=rs))
     model = lowpass_to_lowpass(prototype, wo=wp)
     model = discretize(model, dt=1/fs)
+    print(model)
 
     # Use PSD output to white noise input PSD ratio as response
     window = signal.get_window('blackman', 256)
@@ -113,7 +118,7 @@ def main():
     _, outputs = model.output(input_noise.astype(float), t=None)
     output_noise = outputs.T[0]
     freq, output_noise_power_density = signal.welch(
-        output_noise, fs, window=window, scaling='density')
+        output_noise, fs, window=window)
     expected_response = 10 * np.log10(
         output_noise_power_density /
         input_noise_power_density + 1/inf
@@ -202,7 +207,7 @@ def main():
             output_noise = func(input_noise).T[0]
             _, output_noise_power_density = signal.welch(
                 output_noise.astype(float), fs,
-                window=window, scaling='spectrum')
+                window=window)
             response = 10 * np.log10(
                 output_noise_power_density /
                 input_noise_power_density
@@ -228,7 +233,8 @@ def main():
     only_visualize = True
     if not only_visualize:
         stats = deap.tools.Statistics(
-            key=lambda code: code.fitness.values[4])
+            key=lambda code: code.fitness.values)
+        stats.register('avg', np.mean, axis=0)
         stats.register('med', np.median, axis=0)
         stats.register('min', np.min, axis=0)
         pareto_front = deap.tools.ParetoFront()
@@ -240,10 +246,13 @@ def main():
 
         with open('front.pkl', 'wb') as f:
             pickle.dump(pareto_front, f)
+        with open('logbook.pkl', 'wb') as f:
+            pickle.dump(logbook, f)
     else:
         with open('front.pkl', 'rb') as f:
             pareto_front = pickle.load(f)
-
+        with open('logbook.pkl', 'rb') as f:
+            logbook = pickle.load(f)
     codes = []
     criteria = []
     for code in pareto_front:
@@ -266,6 +275,38 @@ def main():
     memory_size = np.array([crt.size_of_memory for crt in criteria])
     memory_size_in_bytes = memory_size * ProcessingUnit.active().wordlength / 8
 
+    plt.figure()
+    gen, med = logbook.select('gen', 'med')
+    crt = Criteria(*np.array(med).T)
+
+    def fit_most_within_unit_interval(values):
+        values = np.asarray(values)
+        med = np.median(values)
+        mad = np.median(np.absolute(values - med))
+        if not mad:
+            return values
+        return (values - med) / (4. * mad)
+
+    plt.plot(gen, fit_most_within_unit_interval(crt.overflow_margin), label='Med[$M_o$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.underflow_margin), label='Med[$M_u$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.stability_margin), label='Med[$M_s$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.arithmetic_snr), label='Med[$SNR_{arit}$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.frequency_response_error), label='Med[$E_2$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.number_of_adders), label='Med[$N_a$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.number_of_multipliers), label='Med[$N_m$]')
+    plt.plot(gen, fit_most_within_unit_interval(crt.size_of_memory), label='Med[$N_e$]')
+    plt.ylabel('Normalized evolution')
+    plt.xlabel('Generations')
+    plt.ylim([-10, 10])
+    plt.legend(loc='upper right')
+    plt.savefig('population_evolution.png')
+
+    plt.figure()
+    plt.plot(*logbook.select('gen', 'nunfeas'))
+    plt.ylabel('Unfeasibles')
+    plt.xlabel('Generations')
+    plt.savefig('unfeasibles.png')
+
     def pareto2d(ax, x, y):
         ax.scatter(
             x, y, marker='o',
@@ -278,22 +319,21 @@ def main():
     ax = fig.add_subplot(3, 1, 1)
     idx = solvers.gp.argnondominated(-frequency_response_error, stability_margin)
     pareto2d(ax, frequency_response_error[idx], stability_margin[idx])
-    ax.set_xlabel('Frequency Response Error [dB]')
-    ax.set_ylabel('Stability Margin (log)')
+    ax.set_ylabel('Margin $M_s$')
     ax.invert_xaxis()
     ax = fig.add_subplot(3, 1, 2)
     idx = solvers.gp.argnondominated(-frequency_response_error, arithmetic_snr)
     pareto2d(ax, frequency_response_error[idx], arithmetic_snr[idx])
-    ax.set_xlabel('Frequency Response Error [dB]')
-    ax.set_ylabel('Arithmetic SNR [dB]')
+    ax.set_ylabel('$SNR_{arit}$ [dBr]')
     ax.invert_xaxis()
     ax = fig.add_subplot(3, 1, 3)
     idx = solvers.gp.argnondominated(-frequency_response_error, -memory_size_in_bytes)
     pareto2d(ax, frequency_response_error[idx], memory_size_in_bytes[idx])
-    ax.set_xlabel('Frequency Response Error [dB]')
-    ax.set_ylabel('Memory size [bytes]')
+    ax.set_xlabel('Error $E_2$ [dBr]')
+    ax.set_ylabel('Total $N_e$ @ Q15 [bytes]')
     ax.invert_xaxis()
     ax.invert_yaxis()
+    plt.savefig('pareto_fronts.png')
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -308,41 +348,58 @@ def main():
         c=stability_margin[idx],
         cmap='jet_r'
     )
-    fig.colorbar(scatter, ax=ax, label='Stability margin')
-    ax.set_xlabel('Frequency Response Error [dB]')
-    ax.set_ylabel('Arithmetic SNR [dB]')
-    ax.set_zlabel('Memory size [bytes]')
+
+    fig.colorbar(scatter, ax=ax, label='Margin $M_s$')
+    ax.set_xlabel('Error $E_2$ [dBr]')
+    ax.set_ylabel('$SNR_{arit}$ [dBr]')
+    ax.set_zlabel('Total $N_e$ @ Q15 [bytes]')
     ax.invert_xaxis()
     ax.invert_zaxis()
+    plt.savefig('pareto_sampling.png')
 
-    index = np.argsort(frequency_response_error)[2]
+    index = np.argsort(frequency_response_error)[0]
     implement = toolbox.compile(codes[index])
-    optimized_implementation = implement(prototype)
+    optimized_implementation_a = implement(prototype)
+    index = np.argsort(frequency_response_error)[3]
+    implement = toolbox.compile(codes[index])
+    optimized_implementation_b = implement(prototype)
 
-    pretty(optimized_implementation).draw('optimized.svg')
+    print(memory_size_in_bytes[np.argsort(frequency_response_error)])
+    print(stability_margin[np.argsort(frequency_response_error)])
+    print(arithmetic_snr[np.argsort(frequency_response_error)])
 
-    func = signal_processing_function(optimized_implementation)
+    pretty(optimized_implementation_a).draw('optimized_a.png')
+    pretty(optimized_implementation_b).draw('optimized_b.png')
+
+    func = signal_processing_function(optimized_implementation_a)
     output_noise = func(input_noise).T[0]
     _, output_noise_power_density = signal.welch(
         output_noise.astype(float), fs,
         window=window, scaling='density')
-    optimized_implementation_response = \
+    optimized_implementation_a_response = \
         10 * np.log10(output_noise_power_density / input_noise_power_density + 1/inf)
 
-    show_biquad_cascade = False
+    func = signal_processing_function(optimized_implementation_b)
+    output_noise = func(input_noise).T[0]
+    _, output_noise_power_density = signal.welch(
+        output_noise.astype(float), fs,
+        window=window, scaling='density')
+    optimized_implementation_b_response = \
+        10 * np.log10(output_noise_power_density / input_noise_power_density + 1/inf)
+
+    show_biquad_cascade = True
     if show_biquad_cascade:
         biquad_cascade = series_diagram([
             DirectFormI.from_model(
                 signal.dlti(section[:3], section[3:], dt=model.dt), dtype=fixed
             ) for section in signal.zpk2sos(model.zeros, model.poles, model.gain)
-        ])
+        ], simplify=False)
 
-        # pretty(biquad_cascade).draw('result.svg')
+        pretty(biquad_cascade).draw('biquad.png')
         func = signal_processing_function(biquad_cascade)
         output_noise = func(input_noise).T[0]
         _, output_noise_power_density = signal.welch(
-            output_noise.astype(float), fs,
-            window=window, scaling='density')
+            output_noise.astype(float), fs, window=window)
         biquad_cascade_response = \
             10 * np.log10(output_noise_power_density / input_noise_power_density + 1/inf)
 
@@ -350,10 +407,13 @@ def main():
     plt.plot(freq, expected_response, label='Model')
     if show_biquad_cascade:
         plt.plot(freq, biquad_cascade_response, label='Biquad cascade')
-    plt.plot(freq, optimized_implementation_response, label='Optimized')
-    plt.xlabel('Frequency [Hz]')
-    plt.ylabel('Magnitude Response [dB]')
+        pass
+    plt.plot(freq, optimized_implementation_a_response, label='Optimized realization A')
+    plt.plot(freq, optimized_implementation_b_response, label='Optimized realization B')
+    plt.xlabel('Frecuency $f$ [Hz]')
+    plt.ylabel('Response $|H(f)|$ [dBr]')
     plt.legend()
+    plt.savefig('response.png')
     plt.show()
 
 
